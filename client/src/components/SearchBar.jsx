ae99908930f21b75ec1ea982526997c4a3e2aae4
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-08-09 22:20:32 +0200

    searchbar fixed/home page responsive done

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <div className="max-w-md mx-auto">
-      <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-        <div className="grid place-items-center h-full w-12 text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
+    <div className="flex items-center justify-center">
+      <form method="GET">
+        <div className="relative text-gray-600 focus-within:text-gray-400">
+          <span className="rounded-md absolute inset-y-0 left-0 flex items-center pl-2 bg-gray-800 ">
+            <button type="submit" className=" pr-5 focus:outline-none focus:shadow-outline">
+              <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6 ml-3">
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </button>
+          </span>
+          <input
+            type="text"
+            name="q"
+            className="indent-0 py-4 px-12 md:px-16 lg:px-20 text-xl text-white bg-gray-900 rounded-md pl-16 focus:outline-none focus:bg-white focus:text-gray-900 w-full md:w-auto"
+            placeholder="Search..." 
+            autoComplete="off"
+          />
         </div>
-
-        <input
-          className="peer h-full w-full outline-none text-sm text-gray-900 pr-2"
-          type="search" // Use type="search" to avoid showing search history
-          id="search"
-          placeholder="Search..."
-        />
-      </div>
+      </form>
     </div>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import SearchBar from '../components/SearchBar';
-import MovieList from '../components/MovieList';
+import MovieCard from '../components/MovieCard';
+
 const Home = () => {
   return (
     <div className="bg-gradient-to-r from-gray-800 via-blue-500 to-violet-500">
       <Navbar />
-      <div >
-        <h1 className="text-center decoration-8 text-8xl font-bold p-32">Watch <br></br> <h2 className=" text-transparent  bg-clip-text bg-gradient-to-t from-sky-100 via-cyan-200 to-sky-100">Movies and TV Shows</h2><h4 className=" text-transparent  bg-clip-text bg-gradient-to-t from-sky-300 via-cyan-300 to-sky-300">Cinema24</h4></h1>
+      <div className="text-center mt-10 md:mt-20 p-8 md:p-16 lg:p-24">
+        <h1 className="text-4xl md:text-6xl lg:text-9xl font-bold">
+          Watch
+        </h1>
+        <h2 className="font-bold text-transparent bg-clip-text bg-gradient-to-t from-sky-100 via-cyan-200 to-sky-100 text-4xl md:text-6xl lg:text-9xl mt-2">
+          Movies and TV Shows
+        </h2>
+        <h4 className="font-bold text-transparent bg-clip-text bg-gradient-to-t from-sky-300 via-cyan-300 to-sky-300 text-3xl md:text-4xl lg:text-8xl mt-2">
+          Cinema24
+        </h4>
       </div>
-      <SearchBar />
-      <MovieList />
+
+      <div className="flex justify-center items-center p-4 md:p-8 lg:p-16">
+        <div className="w-full max-w-xl">
+          <SearchBar />
+        </div>
+      </div>
+
+      <MovieCard />
       <Footer />
-      
     </div>
   );
 };
