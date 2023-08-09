1b3c4da4fbb8099055e503edb31b672cea814770
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-08-09 17:49:04 +0200

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
         <h1 className="text-center decoration-8 text-8xl font-bold p-32">Watch <br></br> <h2 className=" text-transparent  bg-clip-text bg-gradient-to-t from-sky-100 via-cyan-200 to-sky-100">Movies and TV Shows</h2><h4 className=" text-transparent  bg-clip-text bg-gradient-to-t from-sky-300 via-cyan-300 to-sky-300">Cinema24</h4></h1>
       </div>
       <SearchBar />
-      
-      <Footer />
       <MovieList />
+      <Footer />
+      
     </div>
   );
 };
